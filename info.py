8252d67012d34512f42d2e4d87f174826f48fc2c
Umesh Gurjar <95861754+Umyagurjar@users.noreply.github.com>
2024-12-31 19:46:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,8 +49,8 @@ OWNER_USER_NAME = environ.get("OWNER_USER_NAME", "UmeshGurjar1390") # widout ðŸ‘
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002232612097').split()]
 # post channel auto post new movie
 POST_CHANNELS = list(map(int, (channel.strip() for channel in environ.get('POST_CHANNELS', '-1002232612097').split(','))))
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '0'))
-AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '0'))
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002296972913'))
+AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002296972913'))
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), True)
 
 # MongoDB information
