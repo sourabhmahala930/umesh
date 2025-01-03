30e35563380539d725910271cfab07eee6f9dd97
Umesh Gurjar <95861754+Umyagurjar@users.noreply.github.com>
2025-01-03 10:44:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 # post channel auto post new movie
 POST_CHANNELS = list(map(int, (channel.strip() for channel in environ.get('POST_CHANNELS', '-1002232612097').split(','))))
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002296972913'))
-AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002296972913'))
+AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '0'))
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), True)
 
 # MongoDB information
